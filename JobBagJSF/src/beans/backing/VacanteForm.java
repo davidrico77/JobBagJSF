f9391db99c42a2dd23f695595a7f9ba7421f29bf
davidrico77 <davidrico77@gmail.com>
2018-03-05 00:48:15 +0100

    add beans

---
+++ JobBagJSF/src/beans/backing/VacanteForm.java
@@ -0,0 +1,56 @@
+package beans.backing;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import beans.model.Candidato;
+
+@RequestScoped
+@ManagedBean
+//@Named
+public class VacanteForm {
+
+    Logger log = LogManager.getRootLogger();
+
+    //@Inject
+    @ManagedProperty(value = "#{candidato}")
+    private Candidato candidato;
+
+    public VacanteForm() {
+        log.info("Creando objeto VacanteForm");
+    }
+
+    public void setCandidato(Candidato candidato) {
+        this.candidato = candidato;
+    }
+
+    //Metodo de flujo de control
+    public String enviar() {
+
+        System.out.println("enviar() Nombre=" + this.candidato.getNombre());
+        System.out.println("enviar() Apellido=" + this.candidato.getApellido());
+        System.out.println("enviar() Sueldo deseado" + this.candidato.getSueldoDeseado());
+
+        if (this.candidato.getNombre().equals("Juan")) {
+
+            if (this.candidato.getApellido().equals("Perez")) {
+                String msg = "Gracias, pero Juan Perez ya trabaja con nosotros.";
+                FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg);
+                FacesContext facesContext = FacesContext.getCurrentInstance();
+                String clientId = null; //Este es un mensaje global
+                facesContext.addMessage(clientId, facesMessage);
+                return "index";
+            }
+            return "exito";//exito.xhtml
+        } else {
+            return "fallo"; //fallo.xhtml
+        }
+    }
+
+}
